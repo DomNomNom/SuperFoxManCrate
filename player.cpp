2ee300d65518405f481a76c344907d0e9c9e4c25
Dominik Schmid <DominikSchmid93@gmail.com>
2011-07-09 17:15:31 +1200

    Perfected movement controll.

--- player.cpp
+++ player.cpp
@@ -3,11 +3,10 @@
 #include "player.hpp"
 #include "utils.hpp"
 
-#define PLAYER_WALKSPEED 0.1
-#define PLAYER_GRAVITY 0.001
+#define PLAYER_WALKSPEED 0.3
+#define PLAYER_GRAVITY 0.002
+#define PLAYER_JUMP_STRENGTH 0.03
 #define PLAYER_SIZE 16
-#define PLAYER_GROUND_ACCELERATION 0.0005
-#define PLAYER_AIR_ACCELERATION 0.0001
 
 Player::Player (float x, float y, const sf::Image &image) {
   pos.x=x; pos.y=y;
@@ -15,13 +14,13 @@ Player::Player (float x, float y, const sf::Image &image) {
   dV.x =0; dV.y =0;
   inAir = true;
   img.SetImage(image);
-  //img.SetBlendMode(sf::Blend::Mode(2));
+  img.SetBlendMode(sf::Blend::Mode(3));
 }
 
 void Player::checkKeys(sf::RenderWindow& rw) {
   if (rw.GetInput().IsKeyDown(sf::Key::Up)) {
     if (!inAir) { 
-      dV.y -= 20*PLAYER_GRAVITY; 
+      dV.y -= PLAYER_JUMP_STRENGTH; 
       inAir = true; 
     }
     cancleJump = false;
@@ -32,28 +31,12 @@ void Player::checkKeys(sf::RenderWindow& rw) {
 }
 
 void Player::update(float dt) {
-  if (inAir) currentAcc = PLAYER_AIR_ACCELERATION;
-  else currentAcc = PLAYER_GROUND_ACCELERATION;
-  
-  if (dV.x==0&&false) {
-    if (vel.x>0) dV.x = -currentAcc;
-    else if(vel.x<0) dV.x = currentAcc;
-    
-    if (abs(vel.x + dV.x*dt) < currentAcc) dV.x = -vel.x/(0.1+dt); // make it zero if close to zero
-  }
-  else dV.x *= currentAcc;
-  std::cout << vel.x << "\t/\t" << dt << "\t=\t" << vel.x/dt << std::endl;
   
   dV.y += PLAYER_GRAVITY;
 
   // calculate vel
-  vel += dV*30.f; // ZZZZZZZZZZZZ
-  if (dV.x==0) {
-    vel.x *= 1-(currentAcc);//*11000;
-  }
-  
-    if (vel.x > PLAYER_WALKSPEED) vel.x = PLAYER_WALKSPEED;
-    else if (vel.x < -PLAYER_WALKSPEED) vel.x = -PLAYER_WALKSPEED;
+  vel += dV*dt;
+  vel.x = dV.x * PLAYER_WALKSPEED;
   
   
   if (inAir && vel.y<0 && cancleJump) vel.y = 0;  // cancle the jump if possible
