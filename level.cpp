99569428f048e75b58692768b8bd52d468c7cd15
Dominik Schmid <DominikSchmid93@gmail.com>
2011-07-24 12:12:58 +1200

    removed old text file loading

--- level.cpp
+++ level.cpp
@@ -40,17 +40,6 @@ Level::Level(const char *filePath) {
       }
     }
   }
-  
-  /*  // text file loading
-  std::ifstream levelFile;
-  levelFile.open(filePath);
-  if (levelFile.is_open()) {
-    for(int x,y,wd,ht; levelFile>>x && levelFile>>y && levelFile>>wd && levelFile>>ht; ) {   // read the file
-      platforms.push_back(Platform(x*TILE_SIZE, y*TILE_SIZE, wd*TILE_SIZE, ht*TILE_SIZE, platformTexture));  // construct platforms
-    }
-    levelFile.close();
-  }
-  */
 }
 
 bool Level::collidesWith(CollisionObject &p) {
